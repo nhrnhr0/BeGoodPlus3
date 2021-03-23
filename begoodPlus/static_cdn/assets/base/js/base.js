f759987e7a540b52d85f04e57707fa73b24ebe14
nhrnhr0 <ronionsegal@gmail.com>
2021-03-23 17:35:22 +0200

    change progres bar location and add glow to active swiper image

--- begoodPlus/static_cdn/assets/base/js/base.js
+++ begoodPlus/static_cdn/assets/base/js/base.js
@@ -90,7 +90,7 @@ document.addEventListener('scroll', function(e) {
 var check_list_inputs = document.querySelectorAll('.section-2 .check-list ul li input');
 function handleSection2Checkmarks(pos) {
     var precent = 0;
-    if(pos<100) {
+    if(pos<50) {
         precent = 10;
         check_list_inputs[0].checked = false;
         check_list_inputs[1].checked = false;
