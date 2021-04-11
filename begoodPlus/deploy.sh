5da2d9b695386dc96ae5f582ad85d55475c7c000
nhrnhr0 <ronionsegal@gmail.com>
2021-04-11 15:02:24 +0300

    fix deply script

--- begoodPlus/deploy.sh
+++ begoodPlus/deploy.sh
@@ -1,6 +1,5 @@
-sudo git pull
 source ../env/bin/activate
-python manage.py collectstatic -y
+python manage.py collectstatic --noinput
 python manage.py compress --force
 sudo supervisorctl restart all
 sudo service nginx restart
